0117073e81d73fcd1e779982052c6b50eae34a9c
Jiminy Cao <jiminy.cao@gmail.com>
2022-08-03 15:23:15 -0400

    updated prisma model + signup&signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,54 @@
-import { Injectable } from "@nestjs/common";
-import {User, Bookmark} from '@prisma/client'
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { User, Bookmark } from '@prisma/client'
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable({})
-export class AuthService{
-    constructor(private prisma: PrismaService){}
-    signup(dto: AuthDto){
-        return 'signup';
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+
+            delete user.hash;
+
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError){
+                if (error.code === 'P2002'){
+                    throw new ForbiddenException('Email already exists');
+                }
+            }
+            throw error;
+        }
+
     }
-    signin(){
-        return 'signin';
+    
+    async signin(dto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            },
+        });
+        
+        if (!user)
+            throw new ForbiddenException('Invalid credentials');
+
+        const validPassword = await argon.verify(user.hash, dto.password);
+
+        if (!validPassword)
+            throw new ForbiddenException('Invalid credentials');
+
+        delete user.hash;
+
+        return user;
     }
 }
